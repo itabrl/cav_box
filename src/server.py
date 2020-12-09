80a56ff956a9b7d06c59ba87af4a35ee0b49ca31
itabrl <samir.manafzadehtabriz@leidos.com>
2020-12-09 16:10:23 -0500

    Resolve J2735 decode bug inside server

--- src/server.py
+++ src/server.py
@@ -5,6 +5,13 @@ import json
 import sys
 import csv
 
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
+
+BOOTSTRAP_SERVER_IP = '127.0.0.1:9092'
+
+# producer = KafkaProducer(bootstrap_servers=[BOOTSTRAP_SERVER_IP])
+
 class TCPHandler(socketserver.BaseRequestHandler):
     """
     The request handler class for our server.
@@ -12,39 +19,44 @@ class TCPHandler(socketserver.BaseRequestHandler):
     It is instantiated once per connection to the server, and must
     override the handle() method to implement communication to the
     client.
-    """
+    """        
 
     def handle(self):
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
         print("{} wrote:".format(self.client_address[0]))
-        # print(self.data)
 
-        print(unhexlify(self.data).decode('utf-8'))
-
+        msge = unhexlify(self.data).decode('utf-8')
         msg = J2735.DSRC.MessageFrame
-        msg.from_uper(unhexlify(self.data))
+        msg.from_uper(unhexlify(msge))
 
+        print("msg", msg.__dict__)
+
+        try:
+            json_object = json.dumps(msg(), indent = 4) 
+
+            # dict2str=json.dumps(msg(),indent=4,sort_keys=True,ensure_ascii=False)
+            # parsed = json.loads(dict2str.decode("utf-8","ignore"))
+            # parsed = json.loads(dict2str.decode("utf-8","ignore"))
+            # dict2str=json.dumps(msg())
+        except Exception as e:
+            print(e)
+
+        print("trying kafka")
+        try:
+            # Asynchronous by default
+            future = producer.send('my-topic', b'raw_bytes')
+        except Exception as e:
+            print(e)
 
         # txt=b"0014251d59d162dad7de266e9a7d1ea6d4220974ffffffff8ffff080fdfa1fa1007fff0000640fa0"
         # hexed = hexlify(txt)
-
-        # msg.from_uper(unhexlify(hexed))
-
-        # msg.from_uper(self.data)
-
-        # dict2str=json.dumps(msg(),indent=4,sort_keys=True,ensure_ascii=False)
-        # parsed = json.loads(dict2str.decode("utf-8","ignore"))
-
-        print("msg")
-
-        print(msg.__dict__)
 
         # just send back the same data, but upper-cased
         self.request.sendall(self.data.upper())
 
 if __name__ == "__main__":
-    HOST, PORT = "127.0.0.1", 8881
+    HOST, PORT = "127.0.0.1", 8882
 
     # Create the server, binding to localhost on port 9999
     with socketserver.TCPServer((HOST, PORT), TCPHandler) as server:
