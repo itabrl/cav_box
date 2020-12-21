7f9ad27466acb22eb7795e9f497971973265e67d
itabrl <samir.manafzadehtabriz@leidos.com>
2020-12-21 14:32:07 -0500

    Update server.py with correct config parameters and catching errors

--- src/server.py
+++ src/server.py
@@ -4,13 +4,13 @@ import J2735
 import json
 import sys
 import csv
-
+import logging
 from kafka import KafkaProducer
 from kafka.errors import KafkaError
 
 BOOTSTRAP_SERVER_IP = '127.0.0.1:9092'
 
-# producer = KafkaProducer(bootstrap_servers=[BOOTSTRAP_SERVER_IP])
+producer = KafkaProducer(bootstrap_servers=[BOOTSTRAP_SERVER_IP], value_serializer=lambda m: json.dumps(m).encode('ascii'))
 
 class TCPHandler(socketserver.BaseRequestHandler):
     """
@@ -22,38 +22,40 @@ class TCPHandler(socketserver.BaseRequestHandler):
     """        
 
     def handle(self):
+        def on_send_success(record_metadata):
+            logging.info('Publish data to ' + record_metadata.topic)
+
+        def on_send_error(excp):
+            logging.error('Error sending data', exc_info=excp)
+
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
-        print("{} wrote:".format(self.client_address[0]))
+        logging.info("{} Wrote:".format(self.client_address[0]))
 
-        msge = unhexlify(self.data).decode('utf-8')
         msg = J2735.DSRC.MessageFrame
-        msg.from_uper(unhexlify(msge))
 
-        print("msg", msg.__dict__)
+        logging.info('Unhexlify Data')
+        try:
+            unhex_data = unhexlify(unhexlify(self.data).decode('utf-8'))
+        except Exception as e:
+            logging.exception(e)
+
+        logging.info('Loading message from uper')
+        try:
+            msg.from_uper(unhex_data)
+            msg()['value'][1]['coreData']['id'] = str(msg()['value'][1]['coreData']['id'])
+        except Exception as e:
+            logging.exception(e)
 
         try:
-            json_object = json.dumps(msg(), indent = 4) 
-
-            # dict2str=json.dumps(msg(),indent=4,sort_keys=True,ensure_ascii=False)
-            # parsed = json.loads(dict2str.decode("utf-8","ignore"))
-            # parsed = json.loads(dict2str.decode("utf-8","ignore"))
-            # dict2str=json.dumps(msg())
+            # rdm Received DSRC Messages
+            future = producer.send('received-dsrc-messages', value=msg()).add_callback(on_send_success).add_errback(on_send_error)
         except Exception as e:
-            print(e)
-
-        print("trying kafka")
-        try:
-            # Asynchronous by default
-            future = producer.send('my-topic', b'raw_bytes')
-        except Exception as e:
-            print(e)
-
-        # txt=b"0014251d59d162dad7de266e9a7d1ea6d4220974ffffffff8ffff080fdfa1fa1007fff0000640fa0"
-        # hexed = hexlify(txt)
+            logging.exception(e)
 
         # just send back the same data, but upper-cased
         self.request.sendall(self.data.upper())
+
 
 if __name__ == "__main__":
     HOST, PORT = "127.0.0.1", 8882
